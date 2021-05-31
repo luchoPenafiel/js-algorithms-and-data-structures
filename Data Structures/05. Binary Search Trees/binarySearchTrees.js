1c6f999d9cf2cd5253489c08278578edc086af16
Luciano Peñafiel <luciano.penafiel@aerolab.co>
2021-05-31 08:12:28 -0300

    bfs

--- Data Structures/05. Binary Search Trees/binarySearchTrees.js
+++ Data Structures/05. Binary Search Trees/binarySearchTrees.js
@@ -74,6 +74,34 @@ class BinarySearchTree {
       }
     }
   }
+
+  BFS() {
+    let node = this.root;
+    const data = [];
+    const queue = [];
+
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+
+      data.push(node.value);
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+
+    return data;
+  }
 }
 
 let tree = new BinarySearchTree();
+
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+
+tree.BFS();
