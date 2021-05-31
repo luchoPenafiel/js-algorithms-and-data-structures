a38440b1f04c1b688be0368ff80db7529f2389aa
Luciano Peñafiel <luciano.penafiel@aerolab.co>
2021-05-31 08:30:34 -0300

    DFS pre order

--- Data Structures/05. Binary Search Trees/binarySearchTrees.js
+++ Data Structures/05. Binary Search Trees/binarySearchTrees.js
@@ -93,6 +93,21 @@ class BinarySearchTree {
 
     return data;
   }
+
+  DFSPreOrder() {
+    const data = [];
+
+    const traverse = (node) => {
+      data.push(node.value);
+
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    };
+
+    traverse(this.root);
+
+    return data;
+  }
 }
 
 let tree = new BinarySearchTree();
